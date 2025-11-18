632064adb825969d4c4ab53f4e2343cf8fe27aa3
rajini-lifo <ranjini.m@lifotechnologies.com>
2025-11-18 16:17:31 +0530

    refresh page issue

--- app/dashboards/__init__.py
+++ app/dashboards/__init__.py
@@ -12,6 +12,7 @@ def register_dashboards(app):
     from app.dashboards.wcod.crude_quality import create_crude_quality_dashboard
     from app.dashboards.wcod.crude_carbon import create_crude_carbon_dashboard
     from app.dashboards.wcod.crude_comparison import create_crude_comparison_dashboard
+    from app.dashboards.wcod.crude_overview import create_crude_overview_dashboard
     print('app',app)
     # Create Dash apps - each gets its own server instance
     # Dash apps will be accessible at their url_base_pathname
@@ -22,11 +23,13 @@ def register_dashboards(app):
     crude_quality_dash = create_crude_quality_dashboard(app, '/dash/crude-quality/')
     crude_carbon_dash = create_crude_carbon_dashboard(app, '/dash/crude-carbon/')
     crude_comparison_dash = create_crude_comparison_dashboard(app, '/dash/crude-comparison/')
+    crude_overview_dash = create_crude_overview_dashboard(app, '/dash/crude-overview/')
     # Store dash apps on Flask app for reference
     print('country_dash',country_dash)
     print('crude_quality_dash',crude_quality_dash)
     print('crude_carbon_dash',crude_carbon_dash)
     print('crude_comparison_dash',crude_comparison_dash)
+    print('crude_overview_dash',crude_overview_dash)
     app.dash_apps = {
         'wcod': wcod_dash,
         'country-profile': country_dash,
@@ -34,6 +37,7 @@ def register_dashboards(app):
         'exports': exports_dash,
         'crude-quality': crude_quality_dash,
         'crude-carbon': crude_carbon_dash,
-        'crude-comparison': crude_comparison_dash
+        'crude-comparison': crude_comparison_dash,
+        'crude-overview': crude_overview_dash
     }
 
